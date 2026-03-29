df49a46c177da99c377d754eeb21187b4732258e
Hyv <hendrickrodriguez.py@gmail.com>
2026-03-28 22:28:26 -0400

    feat: export createClient from core barrel

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,3 +2,4 @@ export { Ok, Err, OkResult, ErrResult, type Result } from "./result";
 export { createLogger, type Logger } from "./logger";
 export { CooldownManager, SessionManager, LockSet, cooldowns, locks } from "./state";
 export { getDb, getMongoClient, disconnectDb } from "./db";
+export { createClient } from "./client";
